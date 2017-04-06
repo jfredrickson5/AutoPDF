65561e3875998cb4786b93e5b6f0084bba87aeb7
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-04-06 12:52:34 -0700

    add a basic CLI

--- AutoPDF/Program.cs
+++ AutoPDF/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using CsvHelper;
+using System.IO.Compression;
 
 namespace AutoPDF
 {
@@ -11,6 +13,57 @@ namespace AutoPDF
     {
         static void Main(string[] args)
         {
+            var templateFile = args[0];
+            var inputFile = args[1];
+            var destination = args[2];
+            
+            var useZipFile = destination.EndsWith(".zip");
+            
+            var numRecords = File.ReadLines(inputFile).Count();
+
+            using (var textReader = File.OpenText(inputFile))
+            {
+                var csv = new CsvReader(textReader);
+                csv.Configuration.Delimiter = ":";
+                csv.ReadHeader();
+                var pdfDestination = destination;
+                string tempDir = null;
+
+                if (useZipFile)
+                {
+                    tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                    Directory.CreateDirectory(tempDir);
+                    pdfDestination = tempDir;
+                }
+
+                var index = 0;
+                while (csv.Read())
+                {
+                    index++;
+                    var form = new Form(templateFile);
+                    foreach (var fieldName in csv.FieldHeaders)
+                    {
+                        var fieldValue = csv.GetField(fieldName);
+                        form.Fields.Add(fieldName, fieldValue);
+                    }
+                    using (var file = new FileStream(GenerateFilePath(pdfDestination, numRecords, index), FileMode.Create))
+                    {
+                        form.GetStream().WriteTo(file);
+                    }
+                }
+
+                if (useZipFile)
+                {
+                    ZipFile.CreateFromDirectory(tempDir, destination, CompressionLevel.Optimal, false);
+                }
+            }
+        }
+
+        private static string GenerateFilePath(string destination, int numRecords, int index)
+        {
+            var format = new String('0', numRecords.ToString().Length);
+            var filename = index.ToString(format) + ".pdf";
+            return Path.Combine(destination, filename);
         }
     }
 }
