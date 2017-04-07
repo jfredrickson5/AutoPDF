78b3b35e0e1154e12dc26053146260cf53b48e87
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-04-07 08:36:01 -0700

    move bulk of functionality to Filler class

---
+++ AutoPDF/Filler.cs
@@ -0,0 +1,66 @@
+﻿using CsvHelper;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AutoPDF
+{
+    class Filler
+    {
+        private const string InputDelimiter = ":";
+
+        public string TemplateFile { get; set; }
+        public string InputFile { get; set; }
+        public string OutputDirectory { get; set; }
+        public int NumRecords { get; private set; }
+        public string[] FieldNames { get; private set; }
+
+        public Filler(string templateFile, string inputFile, string outputDirectory)
+        {
+            TemplateFile = templateFile;
+            InputFile = inputFile;
+            OutputDirectory = outputDirectory;
+
+            using (var textReader = File.OpenText(InputFile))
+            {
+                var csv = new CsvReader(textReader);
+                csv.Configuration.Delimiter = InputDelimiter;
+                csv.ReadHeader();
+                FieldNames = csv.FieldHeaders;
+            }
+
+            NumRecords = File.ReadLines(InputFile).Count() - 1;
+        }
+        
+        public void Fill()
+        {
+            using (var textReader = File.OpenText(InputFile))
+            {
+                var csv = new CsvReader(textReader);
+                csv.Configuration.Delimiter = InputDelimiter;
+                var index = 0;
+                while (csv.Read())
+                {
+                    index++;
+                    var form = new Form(TemplateFile);
+                    foreach (var fieldName in FieldNames)
+                    {
+                        var fieldValue = csv.GetField(fieldName);
+                        form.Fields.Add(fieldName, fieldValue);
+                    }
+                    using (var file = new FileStream(GenerateFilePath(OutputDirectory, NumRecords, index), FileMode.Create))
+                    {
+                        form.GetStream().WriteTo(file);
+                    }
+                }
+            }
+        }
+        
+        private string GenerateFilePath(string directory, int numRecords, int index)
+        {
+            var format = new String('0', numRecords.ToString().Length);
+            var filename = index.ToString(format) + ".pdf";
+            return Path.Combine(directory, filename);
+        }
+    }
+}
--- AutoPDF/Program.cs
+++ AutoPDF/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using CsvHelper;
+﻿using System.IO;
 using System.IO.Compression;
 
 namespace AutoPDF
@@ -16,59 +10,27 @@ namespace AutoPDF
             var templateFile = args[0];
             var inputFile = args[1];
             var destination = args[2];
-            
             var useZipFile = destination.EndsWith(".zip");
-            
-            var numRecords = File.ReadLines(inputFile).Count();
+            var outputDirectory = destination;
 
-            using (var textReader = File.OpenText(inputFile))
+            if (useZipFile)
             {
-                var csv = new CsvReader(textReader);
-                csv.Configuration.Delimiter = ":";
-                csv.ReadHeader();
-                var pdfDestination = destination;
-                string tempDir = null;
-
-                if (useZipFile)
-                {
-                    tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-                    Directory.CreateDirectory(tempDir);
-                    pdfDestination = tempDir;
-                }
-
-                var index = 0;
-                while (csv.Read())
-                {
-                    index++;
-                    var form = new Form(templateFile);
-                    foreach (var fieldName in csv.FieldHeaders)
-                    {
-                        var fieldValue = csv.GetField(fieldName);
-                        form.Fields.Add(fieldName, fieldValue);
-                    }
-                    using (var file = new FileStream(GenerateFilePath(pdfDestination, numRecords, index), FileMode.Create))
-                    {
-                        form.GetStream().WriteTo(file);
-                    }
-                }
-
-                if (useZipFile)
-                {
-                    if (File.Exists(destination))
-                    {
-                        File.Delete(destination);
-                    }
-                    ZipFile.CreateFromDirectory(tempDir, destination, CompressionLevel.Optimal, false);
-                    Directory.Delete(tempDir, true);
-                }
+                outputDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                Directory.CreateDirectory(outputDirectory);
             }
-        }
 
-        private static string GenerateFilePath(string destination, int numRecords, int index)
-        {
-            var format = new String('0', numRecords.ToString().Length);
-            var filename = index.ToString(format) + ".pdf";
-            return Path.Combine(destination, filename);
+            var filler = new Filler(templateFile, inputFile, outputDirectory);
+            filler.Fill();
+            
+            if (useZipFile)
+            {
+                if (File.Exists(destination))
+                {
+                    File.Delete(destination);
+                }
+                ZipFile.CreateFromDirectory(outputDirectory, destination, CompressionLevel.Optimal, false);
+                Directory.Delete(outputDirectory, true);
+            }
         }
     }
 }
