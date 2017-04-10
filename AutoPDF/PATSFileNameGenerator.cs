6edee389ad85ac6324c1efb0c03c5c69d33e40d0
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-04-10 13:34:35 -0700

    eliminate dots from middle initials

--- AutoPDF/PATSFileNameGenerator.cs
+++ AutoPDF/PATSFileNameGenerator.cs
@@ -7,9 +7,10 @@ namespace AutoPDF
     {
         public string GenerateFileName(Form currentForm, int currentIndex, int numRecords, string templateFile, string inputFile)
         {
+            // Incoming input files will have the format YEAR_ORG_TIMESTAMP.txt (ex: 2014_ENA_1491500013.txt)
             var lastName = currentForm.Fields["Last"];
             var firstName = currentForm.Fields["First"];
-            var middleName = currentForm.Fields["MI"];
+            var middleName = currentForm.Fields["MI"].ToString().Replace(".", "");
             var timestamp = GetTimestamp(inputFile);
             var fileName = lastName + "_" + firstName + "_" + middleName + "_" + timestamp + ".pdf";
             return fileName;
