1ca0223f2b6f817d5666f19201c4590dbdcb193d
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-04-10 13:26:48 -0700

    implement PATS-specific file name generator

---
+++ AutoPDF/PATSFileNameGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace AutoPDF
+{
+    class PATSFileNameGenerator : IFileNameGenerator
+    {
+        public string GenerateFileName(Form currentForm, int currentIndex, int numRecords, string templateFile, string inputFile)
+        {
+            var lastName = currentForm.Fields["Last"];
+            var firstName = currentForm.Fields["First"];
+            var middleName = currentForm.Fields["MI"];
+            var timestamp = GetTimestamp(inputFile);
+            var fileName = lastName + "_" + firstName + "_" + middleName + "_" + timestamp + ".pdf";
+            return fileName;
+        }
+
+        private string GetTimestamp(string inputFileName)
+        {
+            var baseName = Path.GetFileNameWithoutExtension(inputFileName);
+            return baseName.Split('_').Last();
+        }
+    }
+}
