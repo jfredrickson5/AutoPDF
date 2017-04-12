94a4775eb0be241f5b6e6d10d0af6a1127c208a4
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-04-12 10:14:08 -0700

    update to new interface

--- AutoPDF/PATSFileNameGenerator.cs
+++ AutoPDF/PATSFileNameGenerator.cs
@@ -1,16 +1,17 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace AutoPDF
 {
     class PATSFileNameGenerator : IFileNameGenerator
     {
-        public string GenerateFileName(Form currentForm, int currentIndex, int numRecords, string templateFile, string inputFile)
+        public string GenerateFileName(IDictionary<string, object> formFields, int currentIndex, int numRecords, string templateFile, string inputFile)
         {
             // Incoming input files will have the format YEAR_ORG_TIMESTAMP.txt (ex: 2014_ENA_1491500013.txt)
-            var lastName = currentForm.Fields["Last"];
-            var firstName = currentForm.Fields["First"];
-            var middleName = currentForm.Fields["MI"].ToString().Replace(".", "");
+            var lastName = formFields["Last"];
+            var firstName = formFields["First"];
+            var middleName = formFields["MI"].ToString().Replace(".", "");
             var timestamp = GetTimestamp(inputFile);
             var fileName = lastName + "_" + firstName + "_" + middleName + "_" + timestamp + ".pdf";
             return fileName;
